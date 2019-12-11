9b41de33b8f6d0dac517018021bdbb76d96d5083
hell-hammer666 <47253397+hell-hammer666@users.noreply.github.com>
2019-12-11 10:18:20 -0500

    mosyle config script

---
+++ mosyle_mig.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Optional reconnect to WiFi after enrollment removal
+SSIDINFO="Translation 10 Jay"
+PASSWORDINFO="WeAreGr8"
+# Removing JAMF MDM Profile
+echo "Removing MDM profile..."
+/usr/local/jamf/bin/jamf removeMdmProfile
+sleep 5
+# Removing JAMF Framework
+echo "Removing JAMF Framework"
+jamf removeframework
+sleep 10
+# Removing user profiles left behind
+echo "Removing other profiles left behind..."
+# Exclude system/default accounts
+SkipUsers='Shared\|_\|nobody\|root\|daemon'
+for username in $(dscl . list /Users | grep -v $SkipUsers)
+do
+identifier="$(/usr/bin/profiles -L -U "$username" | awk "/attribute/" | awk '{print $4}')"
+echo "Removing profile: $identifier"
+/usr/bin/profiles -R -p "$identifier" -U "$username"
+done
+sleep 10
+# WiFi
+networksetup -setairportnetwork en0 "$SSIDINFO" "$PASSWORDINFO"
+sleep 20
+#Check for DEP Enrollment
+/usr/libexec/mdmclient dep nag
+sudo profiles renew -type enrollment
+sudo profiles -N
+#!/bin/bash
+
